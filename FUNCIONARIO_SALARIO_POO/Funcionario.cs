6e26ec783786bed9394f0c38521023fe55af91cb
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-12 16:53:31 -0300

    primeiro commit

---
+++ FUNCIONARIO_SALARIO_POO/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FUNCIONARIO_SALARIO_POO
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return (SalarioBruto - Imposto);
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AumentarSalario(double porcent)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcent/100.0);
+        }
+    }
+}
---
+++ FUNCIONARIO_SALARIO_POO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace FUNCIONARIO_SALARIO_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            f1.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f1.Imposto = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($"Funcionario {f1}");
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine());
+            f1.AumentarSalario(porcent);
+            Console.WriteLine($"Funcionario {f1}");
+            Console.WriteLine();
+
+
+        }
+    }
+}
